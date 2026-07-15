ea825b59eb31fb4ad78cf1271ec902b1863f4b38
xuzhibo <xzbnb6662005@qq.com>
2026-07-15 15:02:07 +0800

    first commit

---
+++ src/main/java/com/xzb/xialingying/XialingyingApplication.java
@@ -0,0 +1,14 @@
+package com.xzb.xialingying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XialingyingApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(XialingyingApplication.class, args);
+	}
+//yixiugai
+}
